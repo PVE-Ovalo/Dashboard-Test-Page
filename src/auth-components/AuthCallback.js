e86e4ea29b43868fd5f7dfde02df54e3319c326c
Pierre Verbert <pv@ovalo.fr>
2025-03-13 11:00:46 +0100

    First deploy app

--- src/auth-components/AuthCallback.js
+++ src/auth-components/AuthCallback.js
@@ -7,13 +7,27 @@ const AuthCallback = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
+/*  useEffect(() => {
     const code = searchParams.get("code");
     if (code) {
       exchangeCodeForToken(code);
       navigate("/dashboard"); // Redirige vers le tableau de bord
     }
   }, [searchParams, exchangeCodeForToken, navigate]);
+
+*/
+
+useEffect(() => {
+  
+  const urlParts = window.location.href.split("#");
+  const baseUrl = urlParts[0]; // Partie avant le #
+  const urlParams = new URLSearchParams(new URL(baseUrl).search);
+  const code = urlParams.get("code");
+  if (code) {
+    exchangeCodeForToken(code);
+    navigate("/dashboard"); // Redirige vers le tableau de bord
+  }
+}, [searchParams, exchangeCodeForToken, navigate]);
 
   return <div>Connexion en cours...</div>;
 };
